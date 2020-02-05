e4d299a58942124f9d1c7ef0dd3e42b2dec9269b
Vilija <Simonaviciene@users.noreply.github.com>
2020-02-05 23:54:24 +0200

    Prideta: vardas pavarde ir gimimo metai, duomenys isvedami i konsole.

---
+++ UzduotisNr1/Library/NameAndSurname.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public struct AsmensDuomenys
+    {
+        public string Vardas, Pavarde;
+        public DateTime GimimoMetai { get; private set; }
+        
+
+        public AsmensDuomenys(string vardas, string pavarde, DateTime gimimoMetai)
+        {
+            Vardas = vardas;
+            Pavarde = pavarde;
+            GimimoMetai = gimimoMetai;
+            
+
+
+        }
+    }
+    //public class NameAndSurname
+    //{
+    //    public string Vardas { get; private set; }
+    //    public string Pavarde { get; private set; }
+
+    //    public NameAndSurname(string vardas, string pavarde)
+    //    {
+    //        Vardas = vardas;
+    //        Pavarde = pavarde;
+    //    }
+    //}
+    
+}
---
+++ UzduotisNr1/UzduotisNr1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace UzduotisNr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NameAndSurname nameAndSurname = new NameAndSurname("Vilija", "Simonaviciene");
+            //Console.WriteLine("Vardas: {0}, \nPavarde: {1}", nameAndSurname.Vardas, nameAndSurname.Pavarde);
+            //Console.ReadLine();
+
+            DateTime dateTime = new DateTime(1989, 01, 25);
+            AsmensDuomenys asmensDuomenys = new AsmensDuomenys("Vilija", "Simonaviciene", dateTime);
+            Console.WriteLine("Vardas: {0}, \nPavarde: {1}, \nGimimo metai: {2} ", asmensDuomenys.Vardas, asmensDuomenys.Pavarde, dateTime.ToString());
+            Console.ReadLine();
+        }
+
+
+    }
+}
