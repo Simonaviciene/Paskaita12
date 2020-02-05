b8f00760e48d1b1c21d5a1ac42a0c82a16a2b179
Vilija <Simonaviciene@users.noreply.github.com>
2020-02-06 00:43:16 +0200

    kur kurti metoda GetAge?ir ka jame galima talpinti?

--- UzduotisNr1/Library/NameAndSurname.cs
+++ UzduotisNr1/Library/NameAndSurname.cs
@@ -10,18 +10,30 @@ namespace Library
     {
         public string Vardas, Pavarde;
         public DateTime GimimoMetai { get; private set; }
+        public DateTime SiandienosData { get; private set; }
         
 
-        public AsmensDuomenys(string vardas, string pavarde, DateTime gimimoMetai)
+        
+
+        public AsmensDuomenys(string vardas, string pavarde, DateTime gimimoMetai, DateTime siandienosData)
         {
             Vardas = vardas;
             Pavarde = pavarde;
             GimimoMetai = gimimoMetai;
-            
-
-
+            SiandienosData = siandienosData;
         }
-    }
+        public static int GetAge()
+        {
+            int metai = 0;
+            metai = DateTime.Now.Year - 
+        }
+   
+    } 
+   
+    
+
+
+    
     //public class NameAndSurname
     //{
     //    public string Vardas { get; private set; }
