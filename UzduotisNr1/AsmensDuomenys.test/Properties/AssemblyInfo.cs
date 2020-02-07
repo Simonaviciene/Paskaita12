efb5eb60cfb8c7d557d0b198a403b4a3aafdbbf5
Vilija <Simonaviciene@users.noreply.github.com>
2020-02-07 10:17:32 +0200

    pridetas testas

---
+++ UzduotisNr1/AsmensDuomenys.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AsmensDuomenys.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("AsmensDuomenys.test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3aebc24-9385-4920-b5ac-acff31f10965")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UzduotisNr1/AsmensDuomenysLibrary/AsmensDuomenys.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsmensDuomenys
+{
+    public struct AsmensDuomenys
+    {
+        public string Vardas { get; private set; }
+        public string Pavarde { get; private set; }
+        public DateTime GimimoMetai { get; private set; }
+        
+
+
+
+        public AsmensDuomenys(string vardas, string pavarde, DateTime gimimoMetai)
+        {
+            Vardas = vardas;
+            Pavarde = pavarde;
+            GimimoMetai = gimimoMetai;
+            
+        }
+        public int GetAge()
+        {
+
+            int metai = 0;
+            metai = (DateTime.Now.Year - GimimoMetai.Year);
+            return metai;
+        }
+
+    }
+    
+     
+    
+         
+
+    //public class NameAndSurname
+    //{
+    //    public string Vardas { get; private set; }
+    //    public string Pavarde { get; private set; }
+
+    //    public NameAndSurname(string vardas, string pavarde)
+    //    {
+    //        Vardas = vardas;
+    //        Pavarde = pavarde;
+    //    }
+    //}
+
+    
+}
